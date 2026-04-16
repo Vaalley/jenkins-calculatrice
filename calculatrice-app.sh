15a2811203b1b962f0a0001ffb51580627de3987
Valentin <61027346+Vaalley@users.noreply.github.com>
2026-04-16 10:34:23 +0200

    Update calculatrice-app.sh

--- calculatrice-app.sh
+++ calculatrice-app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 
 echo "********************************"
+pwd
 echo "CONSTRUCTION DE L'APPLICATION SUR LE PORT 3000"
 docker compose up --detach --build
 echo "********************************"
